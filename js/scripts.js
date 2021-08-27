f8cd6a4c305eb8b54d665971fbf8a22ce5a22390
Scott Hutley <scotthutley1@comcast.net>
2021-08-27 15:05:50 -0700

    Add jumbotron, card, size radio buttons in html

--- js/scripts.js
+++ js/scripts.js
@@ -14,28 +14,35 @@ Pizza.prototype.sizePrice = function() {
   }
 }
 
-// function sizePrice(size) {
-//   let price = 0;
-//   if (size === "Small") {
-//     price += 10;
-//   } else if (size === "Medium") {
-//     price += 12;
-//   } else if (size === "Large") {
-//     price += 14;
-//   }
-//   return price;
-// }
-
 Pizza.prototype.toppingPrice = function() {
   for (let i=0; i<=this.topping.length; i++) {
     this.price += 2;
   }
 }
 
+$Document.ready(function() {
+  $("form#pizzaForm").submit(function(event) {
+    event.preventDefault();
+
+  });
+});
+
 // function toppingPrice(topping) {
-//   let price = 0;
-//   for (let i=0; i < topping[i].length; i++) {
-//     price += 2;
-//   }
-//   return price;
-// }
+  //   let price = 0;
+  //   for (let i=0; i < topping[i].length; i++) {
+    //     price += 2;
+    //   }
+    //   return price;
+    // }
+
+  // function sizePrice(size) {
+  //   let price = 0;
+  //   if (size === "Small") {
+  //     price += 10;
+  //   } else if (size === "Medium") {
+  //     price += 12;
+  //   } else if (size === "Large") {
+  //     price += 14;
+  //   }
+  //   return price;
+  // }
