243449be7ad6d87258e7ca9440acc517bc4b9473
Scott Hutley <scotthutley1@comcast.net>
2021-08-27 11:13:24 -0700

    WIP rewrote sizePrice function, got to work once but can't get it to display in console again

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,15 @@ function Pizza(size, topping) {
   this.price = 0;
 }
 
-Pizza.prototype.sizePrice = function() {
-  if (this.size === "Small") {
-    this.price += 10;
+// Pizza.prototype.sizePrice = function(size) {
+//   if (this.size === "Small") {
+//   return this.price += 10;
+//   }
+// }
+
+function sizePrice() {
+  price = 0;
+  if (size === "Small") {
+    price += 10;
   }
 }
-
-new Pizza = Pizza("Small", ["Pepperoni", "Ham"]);
